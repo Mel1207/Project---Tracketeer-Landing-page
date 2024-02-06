0c66635ca3b9fa6ed9e52d847794366ec8a5ef31
Mel <57514727+Mel1207@users.noreply.github.com>
2024-02-06 17:39:44 +0800

    added section 3 and fix index html details

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Header from "./components/Header"
 import Navigation from "./components/Navigation"
 import Section1 from "./components/Section1"
 import Section2 from "./components/Section2"
+import Section3 from "./components/Section3"
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
           <Header />
           <Section1 />
           <Section2 />
+          <Section3 />
         </div>
       </main>
     </>
