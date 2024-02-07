48dfc0f66b09063aff9cd3419ab071032ff4e722
Mel <57514727+Mel1207@users.noreply.github.com>
2024-02-07 12:06:21 +0800

    added section 5

---
+++ src/components/Section5.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import SectionHeader from './SectionHeader'
+
+const Section5 = () => {
+  return (
+    <div className='section-5'>
+      <SectionHeader sub='articles' subTheme='th-green' layout='center' title='Its all about tracketeer articles'>
+        <p>If you are going to use a passage of Lorem Ipsum, you need to be sure <br /> there isn't anything embarrassing hidden in the middle of text.</p>
+      </SectionHeader>
+      Section5
+    </div>
+  )
+}
+
+export default Section5
